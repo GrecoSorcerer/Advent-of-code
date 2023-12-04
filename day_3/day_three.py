8b0f011776d4aac1beac532285fcea02a40c8a81
ThatSorcerer <Slgreco@buffalo.edu>
2023-12-03 21:48:18 -0500

    Repaired the gondola #5

--- day_3/day_three.py
+++ day_3/day_three.py
@@ -3,6 +3,7 @@
 """day_three.py: Solution to Advent of Code 2023 Day3: Gear Ratios."""
 
 import time
+import numpy as np
 
 __author__ = "Sal Greco"
 __credits__ = ["Eric Wastl", "Sal Greco"]
@@ -14,8 +15,18 @@ symbols = '-@*=%/$#+&'
 schematic =[list(l) for l in open("day_3/input.in").readlines()]
 
 sum = 0
+gear_ratio = 0
 
 def get_window_item(rel_l, rel_s) -> bool:
+    # If the position being checked on the
+    # schematic is out of bounds, return False.
+    if rel_s < 0\
+    or rel_s >= len(schematic[0]):
+        return False
+    if rel_l < 0\
+    or rel_l >= len(schematic):
+        return False
+    
     try:
         # If there is an int relative the the symbol
         # at (l,s) then return True
@@ -81,6 +92,10 @@ def account_for_part_at_pos(l, s) -> int:
 
 
 def find_in_window(l, s) -> int:
+    # initialize empty lit of gears
+    gears = []
+    # Grab the current symbol
+    sym = schematic[l][s]
     # initialize empty sum for this window
     sum_in_window = 0
     # check if there are integers in the window
@@ -89,16 +104,60 @@ def find_in_window(l, s) -> int:
                 [get_window_item(l,s-1),                     False, get_window_item(l,s+1)],
                 [get_window_item(l+1,s-1), get_window_item(l+1,s), get_window_item(l+1,s+1)]
             ]
+    
+    # Determine if parts in window count as a gear
+    window_count = 0
+    for line in window:
+        # Count the part entries on a line.
+        lc = line.count(True)
+        # If there are no parts, skip this line.
+        if lc == 0:
+            continue
+        # If the part fills a line of a window, it is one part.
+        elif lc == 1\
+          or lc == 3:
+            window_count += 1
+        # If there are 2 parts entries on a line,
+        # and they are next to each other, its 1 part.
+        elif lc == 2\
+        and  line[0] != line[2]:
+            window_count += 1
+        # If there is a gap between the parts there are two parts.
+        else:
+            window_count +=2
+
+        
+
     # get eash line from the window with its relative index,
     for rel_l,line in enumerate(window,-1):
         # get each symbol with its relative index
         for rel_s,item in enumerate(line,-1):
             # check if item in window is True (int in this position)
             if item:
+                # Get the symbol relative to the current symbol
+                rel_sym = schematic[l+rel_l][s+rel_s]
+                # Get the part number AND clear it from the line as dots.
+                part_number = int(account_for_part_at_pos(l+rel_l, s+rel_s))
+                # If the current symbol denotes a possible gear,
+                # and there are only two parts,
+                # where there is a distict part.
+                # Unless we already have two gears.
+                if sym == '*' and rel_sym != '.' and\
+                   window_count == 2 and\
+                   len(gears)<2:
+                    # Add the part to the list of gears
+                    gears += [part_number]
+                    # print(gears)
                 # grab the entire integer and add it to the sum.
-                sum_in_window += int(account_for_part_at_pos(l+rel_l, s+rel_s))
-    # return the sum of values within the window
-    return sum_in_window
+                sum_in_window += part_number
+    # If there are gears, take their product.
+    # Otherwise return 0
+    if gears:
+        gear_ratio_in_window = np.prod(gears)
+    else:
+        gear_ratio_in_window = 0
+
+    return sum_in_window, gear_ratio_in_window
 
 
 st = time.monotonic()
@@ -107,12 +166,13 @@ for l,line in enumerate(schematic,0):
     for s,symb in enumerate(line):
         if symb in symbols:
 
-            ans = int(find_in_window(l,s))
+            sum_ans,gear_ratio_ans = find_in_window(l,s)
 
             # print(ans)
 
-            sum += ans 
+            sum += sum_ans
+            gear_ratio += gear_ratio_ans
 
 et = time.monotonic()
 
-print(sum,et-st)
+print(sum,gear_ratio,et-st)
